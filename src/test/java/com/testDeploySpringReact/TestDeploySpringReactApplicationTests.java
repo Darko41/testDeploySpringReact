15211842143c13f94c1e0a25222a1faa4fca9a66
Darko41 <kracunovicd@icloud.com>
2025-01-20 22:16:52 +0100

    first commit

---
+++ src/test/java/com/testDeploySpringReact/TestDeploySpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testDeploySpringReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeploySpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
